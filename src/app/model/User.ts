accc2a00f594a4fdbe2662ec8d47ae25fca23d4e
Avijit Pal <56922114+Avijit-ap@users.noreply.github.com>
2024-12-01 14:37:03 +0000

    Add mongoose dependency and create User model with message schema adn revert back the app to use src

---
+++ src/app/model/User.ts
@@ -0,0 +1,70 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyingCode: string;
+    verfifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+}
+
+const User: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+
+    },
+    verifyingCode: {
+        type: String,
+        required: [true, "Verifying code is required"],
+    },
+    verfifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verifying code expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", User))
+
+export default UserModel;
