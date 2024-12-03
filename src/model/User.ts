ded328a8f847d1da96f91dfdf5a05dea45a62312
Avijit Pal <56922114+Avijit-ap@users.noreply.github.com>
2024-12-03 13:52:05 +0000

    Add user registration API with email verification and password hashing

--- src/model/User.ts
+++ src/model/User.ts
@@ -20,8 +20,8 @@ export interface User extends Document {
     username: string;
     email: string;
     password: string;
-    verifyingCode: string;
-    verfifyCodeExpiry: Date;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
     isVerified: boolean;
     isAcceptingMessage: boolean;
     messages: Message[];
