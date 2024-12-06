cbee9381df1dcec11debac6597256db9647071d0
Avijit Pal <56922114+Avijit-ap@users.noreply.github.com>
2024-12-06 13:51:38 +0000

    Add AuthProvider context and enhance sign-in button styling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
@@ -13,7 +14,7 @@ export default function Component() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button className="bg-orange-500 m-4 px-2 py-2 rounded " onClick={() => signIn()}>Sign in</button>
     </>
   )
 }
